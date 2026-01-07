c7c46e96ca51aafe3ac356525d747dce08f9163d
ParkChanWoo0321 <op9563_@naver.com>
2026-01-07 22:17:52 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/hcp/HcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
